f181fd126ccc1f6f795a76a7968bee357ba426db
Lucas Martin Luzzani <lucasluzzani@gmail.com>
2022-09-21 18:41:47 -0300

    varios

--- api/src/routesFunction/index.js
+++ api/src/routesFunction/index.js
@@ -7,7 +7,7 @@ const { API_KEY6 } = process.env;
 const getInfoApi = async () => {
   const apiInfo = await axios.get(
     //`https://run.mocky.io/v3/84b3f19c-7642-4552-b69c-c53742badee5`
-    `https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY6}&number=100&addRecipeInformation=true`
+    `https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY3}&number=100&addRecipeInformation=true`
   );
   const apiData = await apiInfo.data.results.map((e) => {
     return {
@@ -40,7 +40,7 @@ const getInfoDB = async () => {
 
 const getApiInfoById = async (id) => {
   return await axios.get(
-    `https://api.spoonacular.com/recipes/${id}/information?apiKey=${API_KEY6}`
+    `https://api.spoonacular.com/recipes/${id}/information?apiKey=${API_KEY3}`
   );
 };
 
