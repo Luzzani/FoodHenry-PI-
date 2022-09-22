322b71976acf7490b6dd1319252c6a4a3d908303
Lucas Martin Luzzani <lucasluzzani@gmail.com>
2022-09-21 21:53:42 -0300

    fixing bugs

--- api/index.js
+++ api/index.js
@@ -34,7 +34,7 @@ const dietsDb = [
 ];
 // Syncing all the models at once.
 conn
-  .sync({ force: false })
+  .sync({ force: true })
   .then(() => {
     server.listen(process.env.PORT, () => {
       console.log("%s listening at 3001"); // eslint-disable-line no-console
