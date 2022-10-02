5a6a1b427daab3ba8922fbc29fca4a5d9db489a3
Lucas Martin Luzzani <lucasluzzani@gmail.com>
2022-10-02 15:25:56 -0300

    deploy

--- api/src/db.js
+++ api/src/db.js
@@ -1,37 +1,18 @@
-require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
+const { DATABASE_URL } = process.env;
 
-let sequelize =
-  process.env.NODE_ENV === "production"
-    ? new Sequelize({
-        database: DB_NAME,
-        dialect: "postgres",
-        host: DB_HOST,
-        port: 5432,
-        username: DB_USER,
-        password: DB_PASSWORD,
-        pool: {
-          max: 3,
-          min: 1,
-          idle: 10000,
-        },
-        dialectOptions: {
-          ssl: {
-            require: true,
-            // Ref.: https://github.com/brianc/node-postgres/issues/2009
-            rejectUnauthorized: false,
-          },
-          keepAlive: true,
-        },
-        ssl: true,
-      })
-    : new Sequelize(
-        `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/food`,
-        { logging: false, native: false }
-      );
+const sequelize = new Sequelize(DATABASE_URL, {
+  logging: false,
+  native: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthor: false,
+    },
+  },
+});
 
 const basename = path.basename(__filename);
 
@@ -71,3 +52,32 @@ module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
   conn: sequelize, // para importart la conexión { conn } = require('./db.js');
 };
+
+// let sequelize =
+//   process.env.NODE_ENV === "production"
+//     ? new Sequelize({
+//         database: DB_NAME,
+//         dialect: "postgres",
+//         host: DB_HOST,
+//         port: 5432,
+//         username: DB_USER,
+//         password: DB_PASSWORD,
+//         pool: {
+//           max: 3,
+//           min: 1,
+//           idle: 10000,
+//         },
+//         dialectOptions: {
+//           ssl: {
+//             require: true,
+//             // Ref.: https://github.com/brianc/node-postgres/issues/2009
+//             rejectUnauthorized: false,
+//           },
+//           keepAlive: true,
+//         },
+//         ssl: true,
+//       })
+//     : new Sequelize(
+//         `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/food`,
+//         { logging: false, native: false }
+//       );
